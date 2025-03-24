af35410c59caa7a08ce6714b3bd9b7dde92f5b88
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-24 13:04:23 -0700

    Update C#Moment.cs

--- C#Moment.cs
+++ C#Moment.cs
@@ -1,5 +1,4 @@
 // Basic C# education moment
-
 // C# is a coding language built off of code "blocks", or callable chunks of code.
 
 // They are ordered in a similar fashion to this:
@@ -15,6 +14,7 @@
   // Bool: A bool (or boolean), is your basic-ass true/false, 1/0, y/n type thing. It's a variable type that pretty much can be true or false, and will be referenced as such.
     // For example, one can reference a bool (example will be named TestBool) by: "if (TestBool == true) or, the simpler way, "if (TestBool)"
     // Bools will innately be translated into a true or false by name, (TestBool is true, !TestBool is false), saving you some time.
+    // You can also do a bool "switch", which is "TestBool = !TestBool". This will switch the bool's current state (true --> false, false --> true)
     
   // String: A string is a line of text. They are generally formatted in a way of $"This is example text from a string". They can be used to lots of things, like UIDs, object descriptions, and saved data like usernames.
 
@@ -37,7 +37,70 @@
     // Foreach loops: foreach loops (like the name suggests) will loop over EACH thing as specified. For example, you can have a Foreach loop check every single entity with a certain component or tag.
     // While loops: While loops will continuously run until it's goal is reached. Example being "while (ExampleFloat <= 10f)". This will continously loop until ExampleFloat is greater than or equal to 10.
       
-  # Exit commands: Exit commands end code when needed, either after a error or incorrect check, or other circumstances. There are three basic ones:
-    # Return: Return will instantly kill a block of code, and prevent it from getting any further down the block until it is re-executed. Useful for stopping code after a known error or incorrect circumstance.
-    # Break: Will "break" a loop, stopping it from continuing and canceling it. Useful if you've completed your goal with the loop and it no longer needs to exist.
-    # Continue: Continue will SKIP an interation in a loop. Instead of breaking it, this instead will just cancel the current iteration and start on the next one. Useful for Foreach loops if you want to skip over certain entities.
+  // Exit commands: Exit commands end code when needed, either after a error or incorrect check, or other circumstances. There are three basic ones:
+    // Return: Return will instantly kill a block of code, and prevent it from getting any further down the block until it is re-executed. Useful for stopping code after a known error or incorrect circumstance.
+    // Break: Will "break" a loop, stopping it from continuing and canceling it. Useful if you've completed your goal with the loop and it no longer needs to exist.
+    // Continue: Continue will SKIP an interation in a loop. Instead of breaking it, this instead will just cancel the current iteration and start on the next one. Useful for Foreach loops if you want to skip over certain entities.
+
+  // Comments: This while document is pretty much a comment. It's a note in code that is ignored by the system. Start comments with "//". Useful for documentation.
+
+// Here's a full example class using stuff we've learned so far:
+
+public class ExampleClass
+{
+
+private void FinalExampleBlock
+{
+  // Create some floats
+  float TestFloat = 0f;
+  float TestFloat2 = 10f;
+
+  // Make a while loop that constantly increases TestFloat by 1 until it's 10.
+  while (TestFloat < TestFloat2)
+  {
+    TestFloat += 1f;
+    // Check if it's 10 or higher.
+    if (TestFloat >= TestFloat2)
+    {
+      // Go to the "GoNext" block
+      GoNext(TestFloat);
+      // Break the loop (it would end on it's own by now, though.)
+      break;
+    }
+  }
+}
+
+private void GoNext (float TestFloat)
+{
+  // Make a var that is equal to testfloat.
+  var TestVar = TestFloat
+  // Multiplies Testvar by TestFloat (pretty much squaring it)
+  TestVar *= TestFloat
+  // Goes to TestFloatCheck (a boolean block)
+  TestFloatCheck(TestVar, TestFloat);
+
+  // If the TestFloatCheck boolean returns true, yay!
+  if (TestFloatCheck)
+  {
+    // We did it!
+    return;
+  }
+}
+
+// A boolean block, which can return true or false
+private bool TestFloatCheck (var TestVar, float TestFloat)
+{
+  // Is TestVar larger than TestFloat?
+  if (TestVar > TestFloat)
+  {
+    // If it is, this block is "true"!
+    return true;
+  }
+  else
+  {
+    // If not, it's false..
+    return false;
+  }
+}
+}
+  

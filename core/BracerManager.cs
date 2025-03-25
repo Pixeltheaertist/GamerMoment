7a11f86af851b727ea3439ba67702b6d7614988c
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-25 10:37:22 -0700

    Update BracerManager.cs

--- core/BracerManager.cs
+++ core/BracerManager.cs
@@ -10,7 +10,32 @@ public class BracerManager : MonoBehaviour
 	public int Brace3 = 1;
 	public int BracerMode = 1;
 	// Bracer modes range from 1-18, depending on the current combination.
-	
+
+ 	private Dictionary<string, int> bracerCombinationModes = new Dictionary<string, int>();
+
+    void Start()
+    {
+        bracerCombinationModes.Add("1-1-1", 1);
+        bracerCombinationModes.Add("2-1-1", 2);
+        bracerCombinationModes.Add("3-1-1", 3);
+        bracerCombinationModes.Add("1-2-1", 4);
+        bracerCombinationModes.Add("1-3-1", 5);
+        bracerCombinationModes.Add("1-1-2", 6);
+        bracerCombinationModes.Add("1-1-3", 7);
+        bracerCombinationModes.Add("2-2-1", 8);
+        bracerCombinationModes.Add("3-3-1", 9);
+        bracerCombinationModes.Add("2-2-2", 10);
+        bracerCombinationModes.Add("1-2-2", 11);
+        bracerCombinationModes.Add("1-3-2", 12);
+        bracerCombinationModes.Add("1-2-3", 13);
+        bracerCombinationModes.Add("1-3-3", 14);
+        bracerCombinationModes.Add("2-3-1", 15);
+        bracerCombinationModes.Add("2-3-2", 16);
+        bracerCombinationModes.Add("2-3-3", 17);
+        bracerCombinationModes.Add("3-2-1", 18);
+        bracerCombinationModes.Add("3-2-2", 19);
+    }
+    
     public void BraceChange1()
 	{
 		Brace1 += 1;
@@ -19,6 +44,7 @@ public class BracerManager : MonoBehaviour
 			Brace1 = 1;
 		}
 		Debug.Log("Bracer ring 1 changed to state: " + Brace1);
+  		UpdateBracer();
 	}
 	
 	public void BraceChange2()
@@ -29,6 +55,7 @@ public class BracerManager : MonoBehaviour
 			Brace2 = 1;
 		}
 		Debug.Log("Bracer ring 2 changed to state: " + Brace2);
+  		UpdateBracer();
 	}
 	
 	public void BraceChange3()
@@ -39,7 +66,23 @@ public class BracerManager : MonoBehaviour
 			Brace3 = 1;
 		}
 		Debug.Log("Bracer ring 3 changed to state: " + Brace3);
+  		UpdateBracer();
 	}
 	
 	// This will store combinations that the bracer is in, or it's "mode"
+
+ 	public void UpdateBracer()
+    {
+        string combinationKey = $"{Brace1}-{Brace2}-{Brace3}";
+
+        if (bracerCombinationModes.ContainsKey(combinationKey))
+        {
+            BracerMode = bracerCombinationModes[combinationKey];
+            Debug.Log("BracerMode updated to: " + BracerMode);
+        }
+        else
+        {
+            Debug.LogWarning("Bracer combination not found. Current combination: " + combinationKey);
+        }
+    }
 }

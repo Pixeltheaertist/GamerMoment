656655d6c52e72b4acf7ec1a180cd7f22a63bfd4
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-25 09:31:11 -0700

    Update and rename death.cs to DeathController.cs

--- core/DeathController.cs
+++ core/DeathController.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class DeathController : MonoBehaviour
 {
+	public List<Character> characters;
+	
 	public void Death()
 	{
 		foreach (Character target in characters)
 		{
-			Destroy(target) // Removes all characters from the screen
+			Destroy(target.GameObject); // Removes all characters from the screen
 		}
 		
 		// Fade in a GAME OVER screen

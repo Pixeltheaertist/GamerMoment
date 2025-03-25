21f5ecd3fd16a4cde0fc4981f6fa56f5fbbb743f
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-25 09:32:07 -0700

    Update and rename tile.cs to Tile.cs

---
+++ core/Tile.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2Int position;  // Tile position in the grid
+	
+    public bool isWalkable = true;  // Whether this tile can be walked on
+    public bool hasCorpse = false; // Whether this tile has a corpse on it for looting
+    public bool isDeadly = false; // Whether this tile will kill you once stepped on
+    public bool difficultTerrain = false; // Whether this tile is harder to traverse
+    public bool isPit = false; // Whether this tile will only allow flying creatures to safely cross
+}

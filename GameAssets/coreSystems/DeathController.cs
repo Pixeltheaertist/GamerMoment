8dc6b90ce95478e15511e54913649c2e43470cbd
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-26 09:58:20 -0700

    Update DeathController.cs

--- GameAssets/coreSystems/DeathController.cs
+++ GameAssets/coreSystems/DeathController.cs
@@ -18,4 +18,10 @@ public class DeathController : MonoBehaviour
 		
 		// Fade in a GAME OVER screen
 	}
+
+ 	public void AIDeath()
+	{
+		// This will hold what occurrs when an AI character dies. It will spawn a corpse that can be walked on to regain health and energy in your Bracer.
+  		// Do this by giving the tile they die on the "hasCorpse" bool.
+	}
 }

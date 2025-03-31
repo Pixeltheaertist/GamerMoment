6d314896a77e2b14878521fdf113df6344bc4549
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-03-31 09:21:30 -0700

    Update TurnManager.cs

--- GameAssets/coreSystems/TurnManager.cs
+++ GameAssets/coreSystems/TurnManager.cs
@@ -24,7 +24,6 @@ public class TurnManager : MonoBehaviour
 
         if (playerObject != null)
         {
-            Character playerCharacter = playerObject.GetComponent<Character>();
             if (playerCharacter != null)
             {
                 AddCharacter(playerCharacter);
